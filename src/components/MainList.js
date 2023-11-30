07825971e094567b9ff07c41c3eb5f638022aab8
dee chao <chaoserady@gmail.com>
2023-11-29 23:55:13 -0600

    updated data

--- src/components/MainList.js
+++ src/components/MainList.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "./MainList.css";
-import Card from "./Card";
+// import Card from "./Card";
+import { data } from "../data/Data";
 
 const MainList = () => {
   return (
     <div className="mainList_container_main">
-      <h1>Main List</h1>
       <div className="mainList_container_listings">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {data.map((data) => (
+          <div className="mainList_box">
+            <li key={data.id}>
+              <p>{data.year}</p>
+              <p>{data.make}</p>
+              <p>{data.model}</p>
+              <p>{data.trim}</p>
+              <p>{data.price}</p>
+            </li>
+          </div>
+        ))}
       </div>
     </div>
   );
