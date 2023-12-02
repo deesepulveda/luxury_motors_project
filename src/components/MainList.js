8a0cb2eabd65f80b1af873d7e7a6ee192f759390
dee chao <chaoserady@gmail.com>
2023-12-02 17:05:52 -0600

    updated data and added usestate hook to sort data

--- src/components/MainList.js
+++ src/components/MainList.js
@@ -1,20 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MainList.css";
-// import Card from "./Card";
 import { data } from "../data/Data";
 
 const MainList = () => {
+  const [carData, setCarData] = useState(data);
+
+  const newData = (make) => {
+    setCarData(
+      data.filter((item) => {
+        return item.make === make;
+      })
+    );
+  };
+
+  // Uniquely Set/Sort Brand Data from Data
+  const brandsArr = new Set(
+    data
+      .map((item) => {
+        return item.make;
+      })
+      .sort()
+  );
+
+  // Filter Data
+
+  const filterMake = (e) => {
+    const curVal = e.currentTarget.value;
+
+    curVal !== "all"
+      ? setCarData(
+          data.filter((item) => {
+            return item.make === curVal;
+          })
+        )
+      : setCarData(data);
+  };
+
   return (
     <div className="mainList_container_main">
+      <div className="mainList_container_filter">
+        <select
+          name="make"
+          id=""
+          className="filter_box_make"
+          onChange={filterMake}
+        >
+          <option value="all">all</option>
+          {Array.from(brandsArr).map((item, index) => (
+            <option key={index}>{item}</option>
+          ))}
+        </select>
+      </div>
       <div className="mainList_container_listings">
-        {data.map((data) => (
-          <div className="mainList_box">
-            <li key={data.id}>
-              <p>{data.year}</p>
-              <p>{data.make}</p>
-              <p>{data.model}</p>
-              <p>{data.trim}</p>
-              <p>{data.price}</p>
+        {carData.map((item) => (
+          <div className="mainList_box" key={item.id}>
+            <li>
+              <p className="item_year_model">
+                {item.year} {item.make}
+              </p>
+              <p className="item_model">{item.model}</p>
+              <p className="item_trim">{item.trim}</p>
+              <p className="item_price">{item.price}</p>
             </li>
           </div>
         ))}
